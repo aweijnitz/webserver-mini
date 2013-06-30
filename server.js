b710fb0d5731494570ce7250d84ff964f75bf9a2
Anders Weijnitz <anders.weijnitz@gmail.com>
2013-06-30 10:19:44 +0200

    Added port config and a .gitignore file

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
+var config = require('../config.json');
 var connect = require('connect');
+
+var green  = '\033[32m';
+var reset = '\033[0m';
 
 var dir = __dirname;
 var port = 8080;
 
-var green  = '\033[32m';
-var reset = '\033[0m';
+if(config.port)
+  port = parseInt(config.port);
 
 if(process.argv.length >= 3)
 	dir = process.argv[2];
